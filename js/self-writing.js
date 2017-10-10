6b077f7fd81885f2c1028cbaf1536965cc604eb4
Yuliia Shamatiienko <y@ione.software>
2017-10-10 18:38:19 +0200

    Added project

---
+++ js/self-writing.js
@@ -0,0 +1,16 @@
+$(function () {
+    $('#selfwr-text').typed({
+        strings: [
+            "IoT пристрої (прототипування та впровадження)",
+            "Програмне забезпечення IoT",
+            "Програмне забезпечення WEB",
+            "Програмне забезпечення смартфонів"
+        ],
+        typeSpeed: 60,
+        backSpeed: 15,
+        backDelay: 150,
+        loop: true,
+        smartBackspace: true
+    });
+    $('.typed-cursor').remove();
+})
